a1031faf3c06dc5d0c2e08f62606273ea2410211
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2025-08-31 13:49:25 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="p-4">
+      <nav class="flex gap-3">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="p-4">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/auth/dashboard.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineOptions({ name: 'DashboardLak' })
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-main">Dashboard</h1>
+</template>
+
+<style scoped>
+</style>
